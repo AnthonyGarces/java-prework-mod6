7d2f9d0265ac854e4eedef594549d0acda608cfd
Este <anthony.garces1995@gmail.com>
2019-09-22 21:17:24 -0500

    initial code upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+document.getElementById("button1").onclick = function () {
+    document.getElementById("box").style = "height:300px; width:300px; background-color:orange; margin:25px";
+}
+document.getElementById("button2").onclick = function () {
+    document.getElementById("box").style = "height:150px; width:150px; background-color:blue; margin:25px";
+}
+document.getElementById("button3").onclick = function () {
+    document.getElementById("box").style = "height:150px; width:150px; background-color:orange; opacity:0.5; margin:25px"
+}
+document.getElementById("button4").onclick = function () {
+    document.getElementById("box").style = "height:150px; width:150px; background-color:orange; margin:25px";
+}
